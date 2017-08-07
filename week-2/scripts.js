0226ea2658a5a972c364974f45883e16f094d203
Kelsey Nichols <kelseynichols2@gmail.com>
2017-08-07 11:27:12 -0500

    correct solution. need to refactor

--- week-2/scripts.js
+++ week-2/scripts.js
@@ -1,74 +1,78 @@
-document.addEventListener('DOMContentLoaded', goDOM);
+document.addEventListener('DOMContentLoaded', goDOM)
 
 function goDOM () {
-    var btn = document.createElement("button");
-    var buttonText = document.createTextNode('Insert Square');
-    var counter = 0;
+  let btn = document.getElementById('addSquare')
+  btn.addEventListener('click', insertSquare)
 
-    btn.appendChild(buttonText);
-    btn.id = "button1";
-    btn.style.height = "30px";
-    btn.style.margin = "2em auto";
-    btn.style.display = 'block';
-    document.body.appendChild(btn);
+  let squareArray = []
 
-    document.getElementById("button1").addEventListener("click", insertSquare);
+  function insertSquare () {
+    let square = document.createElement('div')
+    square.className = 'square'
+    square.id = squareArray.length + 1
+    square.innerHTML = square.id
+    document.body.appendChild(square)
+    squareArray.push(square)
 
-    function insertSquare() {
-        var square = document.createElement('div');
-        square.style.backgroundColor = 'black';
-        square.style.height = '50px';
-        square.style.width = '50px';
-        square.style.margin = '10px';
-        square.style.display = 'inline-block';
-        square.style.textAlign = 'center';
-        square.style.lineHeight = '50px';
-        square.style.verticalAlign = 'middle';
-        square.className = 'squares';
-        counter ++;
-        square.id = counter;
-        square.innerHTML = counter;
-        document.body.appendChild(square);
+    square.addEventListener('mouseover', showNumber)
+    square.addEventListener('mouseout', hideNumber)
+    square.addEventListener('click', changeBackgroundColor)
+    square.addEventListener('dblclick', removeSquare)
 
-        square.addEventListener('mouseover', displayNumber);
-        square.addEventListener('mouseout', hideNumber);
-        square.addEventListener('click', getRandomColor);
-        square.addEventListener('dblclick', removeSquare);
-
-        function displayNumber() {
-            square.style.color = "white";
-        }
-
-        function hideNumber() {
-            square.style.color = "transparent";
-        }
-
-        function getRandomColor() {
-            var letters = '0123456789ABCDEF';
-            var color = '#';
-            for (var i = 0; i < 6; i++ ) {
-                color += letters[Math.floor(Math.random() * 16)];
-            };    
-            square.style.backgroundColor = color;        
-        };
-
-        function removeSquare(e) {
-            var thisSquare = e.target;
-            var squareNumber = thisSquare.innerHTML;
-            if (squareNumber % 2 == 0) {
-                if (thisSquare.nextElementSibling === null) {
-                    alert ("there's nothing to remove!")
-                    return;
-                }
-                thisSquare.nextElementSibling.remove();
-
-            } else {
-                if (thisSquare.previousElementSibling === btn) {
-                    alert ("can't remove the button!")
-                    return;
-                }
-                thisSquare.previousElementSibling.remove();
-            } 
-        }
+    function showNumber () {
+      square.style.color = 'white'
     }
-};
+
+    function hideNumber () {
+      square.style.color = 'transparent'
+    }
+
+    function changeBackgroundColor () {
+      let letters = '0123456789ABCDEF'
+      let color = '#'
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+      }
+      square.style.backgroundColor = color
+    }
+
+    function reNumber () {
+      let counter = 0
+
+      squareArray.forEach(x => {
+        let squarez = document.getElementById(`${x.id}`)
+
+        counter++
+        if (squarez) {
+          squarez.innerHTML = counter
+        }
+      })
+    }
+
+    function removeSquare (e) {
+      let thisSquare = e.target
+      let squareNumber = thisSquare.innerHTML
+      if (squareNumber % 2 === 0) {
+        // if (thisSquare.nextElementSibling === null) {
+        //   window.alert("there's nothing to remove!")
+        //   return
+        // }
+        thisSquare.nextElementSibling.remove()
+        let arrayIndex = squareArray.indexOf(thisSquare)
+        squareArray.splice(arrayIndex, 1)
+        reNumber()
+
+        // squareArray.shift()
+        // this.squareArray
+        // console.log(squareArray.length)
+        // thisSquare.id = arrayIndex
+      } else {
+        if (thisSquare.previousElementSibling === btn) {
+          window.alert("can't remove the button!")
+          return
+        }
+        thisSquare.previousElementSibling.remove()
+      }
+    }
+  }
+}
